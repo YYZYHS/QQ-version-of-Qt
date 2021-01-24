9d557b2f45b3844d83a2268d4358223787b65ada
YYZYHS <77320657+YYZYHS@users.noreply.github.com>
2021-01-24 19:45:41 +0800

    PART01
    1.最简单的服务器
    2.客户端的登陆界面

--- PART01/TcpClient/login.cpp
+++ PART01/TcpClient/login.cpp
@@ -1,9 +1,8 @@
 ﻿#include "login.h"
 
 Login::Login()
-{
-    setMinimumSize(400,150);
-    setMaximumSize(400,150);
+{  
+    //初始化窗口插件
     logoLabel = new QLabel(this);
     usrLabel = new QLabel(this);
     pwdLabel = new QLabel(this);
@@ -13,7 +12,7 @@ Login::Login()
     pwdLineEdit = new QLineEdit(this);
     loginBtn = new QPushButton(this);
     exitBtn = new QPushButton(this);
-
+    //设置文本
     logoLabel->setText("飞鸽传书");
     usrLabel->setText("用户名:");
     pwdLabel->setText("密码:");
@@ -21,10 +20,13 @@ Login::Login()
     pswfind->setText("找回密码");
     loginBtn->setText("登陆");
     exitBtn->setText("退出");
-
+    //设置logo文本对齐方式（居中）
     logoLabel->setAlignment(Qt::AlignHCenter);
 
+    //栅格布局管理器
+    //定义栅格布局管理器
     QGridLayout *loginLayout = new QGridLayout;
+    //添加插件
     loginLayout->addWidget(logoLabel,0,1);
     loginLayout->addWidget(usrLabel,2,0);
     loginLayout->addWidget(usrLineEdit,2,1);
@@ -34,17 +36,24 @@ Login::Login()
     loginLayout->addWidget(pswfind,4,2);
     loginLayout->addWidget(loginBtn,6,0);
     loginLayout->addWidget(exitBtn,6,2);
-
-
-    loginLayout->setHorizontalSpacing(10);
-    loginLayout->setVerticalSpacing(10);
-    loginLayout->setContentsMargins(10,10,10,10);
-
-    connect(loginBtn,SIGNAL(clicked(bool)),this,SLOT(login()));
-    connect(exitBtn,SIGNAL(clicked(bool)),this,SLOT(close()));
+    //设置管理器间距
+    loginLayout->setHorizontalSpacing(10);//两列之间的间距
+    loginLayout->setVerticalSpacing(10);//两行之间的间距
+    loginLayout->setContentsMargins(10,10,10,10);//设置左侧、顶部、右侧和底部边距
+    //设置窗口大小
+    setMinimumSize(400,150);//窗口最大大小
+    setMaximumSize(400,150);//窗口最小大小
+    //安装布局管理器
     setLayout(loginLayout);
+
+    //设置窗口名和窗口图标
     setWindowTitle("飞鸽传书    登陆");
     setWindowIcon(QIcon(":/Image/Image/217c8d364111f8013a8afe7fc51fe572.png"));
+
+    //设置信号与槽
+    connect(loginBtn,SIGNAL(clicked(bool)),this,SLOT(login()));
+    connect(exitBtn,SIGNAL(clicked(bool)),this,SLOT(close()));
+
 
 
 }
--- PART01/TcpClient/login.h
+++ PART01/TcpClient/login.h
@@ -1,14 +1,16 @@
 ﻿#ifndef LOGIN_H
 #define LOGIN_H
 
-#include <QWidget>
-#include <QDialog>
-#include <QLabel>
-#include <QPushButton>
-#include <QLineEdit>
-#include <QMessageBox>
-#include <QGridLayout>
+//需要包含的类
+#include <QWidget>      //窗口类
+#include <QDialog>      //对话框类
+#include <QLabel>       //标签类
+#include <QPushButton>  //按钮类
+#include <QLineEdit>    //输入行类
+#include <QMessageBox>  //消息类
+#include <QGridLayout>  //栅格布局管理器类
 
+//类的前置声明
 class QLabel;
 class QLineEdit;
 class QPushButton;
@@ -20,17 +22,17 @@ public:
     Login();
     //~Login();
 private:
-    QLabel *usrLabel;
-    QLabel *pwdLabel;
-    QLabel *logoLabel;
-    QLabel *logup;
-    QLabel *pswfind;
-    QLineEdit *usrLineEdit;
-    QLineEdit *pwdLineEdit;
-    QPushButton *loginBtn;
-    QPushButton *exitBtn;
+    QLabel *usrLabel;           //文本：账号
+    QLabel *pwdLabel;           //文本：密码
+    QLabel *logoLabel;          //文本：logo
+    QLabel *logup;              //文本：注册账号（用以日后连接数据库使用）
+    QLabel *pswfind;            //文本：找回密码（用以日后连接数据库使用）
+    QLineEdit *usrLineEdit;     //输入框：账号
+    QLineEdit *pwdLineEdit;     //输入框：密码
+    QPushButton *loginBtn;      //按钮：登陆
+    QPushButton *exitBtn;       //按钮：退出
 private slots:
-    void login();
+    void login();               //登陆进聊天室界面
 
 };
 
