6815620bd4f8f5ec0345f3f12292abd9dbfccf4f
YYZYHS <77320657+YYZYHS@users.noreply.github.com>
2021-01-25 06:57:21 +0800

    PART03
    一次大改，界面搭建完成

---
+++ PART03/TcpClient/chatwidget.h
@@ -0,0 +1,63 @@
+﻿#ifndef CHATWIDGET_H
+#define CHATWIDGET_H
+
+#include <QWidget>
+#include <QTextBrowser>
+#include <QFontComboBox>
+#include <QComboBox>
+#include <QToolButton>
+#include <QTextEdit>
+#include <QTableWidget>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QLabel>
+#include <QString>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QGridLayout>
+#include <QHeaderView>
+
+class QTextBrowser;
+class QFontComboBox;
+class QComboBox;
+class QToolButton;
+class QTextEdit;
+class QTableWidget;
+class QLineEdit;
+class QPushButton;
+class QLabel;
+
+class ChatWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    ChatWidget(QWidget *parent = nullptr);
+    ~ChatWidget();
+private:
+    QTextBrowser *messageBroswer;//信息文字浏览器
+    QFontComboBox *fontComboBox;//字体组合框
+    QComboBox *sizeCombox;//字体大小组合框
+    QToolButton *boldTolBtn;//加粗按钮
+    QToolButton *italicToolBtn;//倾斜按钮
+    QToolButton *underlineToolBtn;//下划线按钮
+    QToolButton *colorToolBtn;//更改字体颜色按钮
+    QToolButton *sendToolBtn;//传输文件按钮
+    QToolButton *saveToolBtn;//保存聊天记录按钮
+    QToolButton *clearToolBtn;//清空聊天记录按钮
+    QTextEdit *messageTextEdit;//消息输入框
+    QTableWidget *userTableWidget;//用户列表
+    QPushButton *sendBtn;//发送按钮
+    QLabel *usrNumlabel;//在线用户数
+    QPushButton *exitBtn;//退出按钮
+    QPushButton *refreshBtn;//刷新按钮
+    QLineEdit *chooseusrNumber;//选择用户编号
+
+private slots:
+
+
+
+
+
+};
+#endif // CHATWIDGET_H
---
+++ PART03/TcpClient/login.h
@@ -0,0 +1,37 @@
+﻿#ifndef LOGIN_H
+#define LOGIN_H
+
+//需要包含的类
+#include <QWidget>      //窗口类
+#include <QDialog>      //对话框类
+#include <QLabel>       //标签类
+#include <QPushButton>  //按钮类
+#include <QLineEdit>    //输入行类
+#include <QMessageBox>  //消息类
+#include <QGridLayout>  //栅格布局管理器类
+
+//类的前置声明
+class QLabel;
+class QLineEdit;
+class QPushButton;
+
+class Login : public QDialog
+{
+    Q_OBJECT
+public:
+    Login();
+    //~Login();
+private:
+    QLabel *IPLabel;           //文本：IP地址
+    QLabel *portLabel;           //文本：端口号
+    QLabel *logoLabel;          //文本：logo
+    QLineEdit *IPLineEdit;     //输入框：IP地址
+    QLineEdit *portLineEdit;     //输入框：端口号
+    QPushButton *loginBtn;      //按钮：登陆
+    QPushButton *exitBtn;       //按钮：退出
+private slots:
+    void login();               //登陆进聊天室界面
+
+};
+
+#endif // LOGIN_H
