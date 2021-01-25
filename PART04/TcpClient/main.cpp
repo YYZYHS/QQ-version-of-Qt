910caa110dd96404bf5ebbe12c82e656cab58ee4
YYZYHS <77320657+YYZYHS@users.noreply.github.com>
2021-01-25 08:49:24 +0800

    PART04-backup

---
+++ PART04/TcpClient/main.cpp
@@ -0,0 +1,22 @@
+﻿#include "chatwidget.h"
+#include "login.h"
+#include "registered.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    ChatWidget w;
+    Login l;
+    Registered r;
+    if(l.exec() == QDialog::Accepted)
+    {
+        if(r.exec() == QDialog::Accepted)
+        {
+            w.show();
+        }
+    }
+
+    return a.exec();
+}
---
+++ PART04/TcpClient/registered.cpp
@@ -0,0 +1,40 @@
+﻿#include "registered.h"
+#include <QDebug>
+
+Registered::Registered()
+{
+    //定义
+    setWindowTitle("注册用户");
+    registered = new QPushButton(this);
+    usrname = new QLineEdit(this);
+    //文本
+    registered->setText(QString("注册"));
+    usrname->setPlaceholderText(QString("请输入用户名"));
+
+    //垂直布局
+    QVBoxLayout *horizontalLayout_1 = new QVBoxLayout;
+    horizontalLayout_1->addWidget(usrname);
+    horizontalLayout_1->addWidget(registered);
+    setLayout(horizontalLayout_1);
+
+    //信号与槽
+    connect(registered,SIGNAL(clicked(bool)),this,SLOT(Register()));
+}
+
+void Registered::Register()
+{
+    qDebug()<<"0";
+    if(1)
+    {
+        qDebug()<<"1";
+        accept();
+        qDebug()<<"2";
+    }
+    else
+    {
+        QMessageBox::warning(this,"警告！","用户名或密码错误！",QMessageBox::Yes);
+//        IPLineEdit->clear();
+//        portLabel->clear();
+//        IPLineEdit->setFocus();
+    }
+}
